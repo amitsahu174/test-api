ccdeb60ab119319c78368dd49dbf9b67166b3c07
amitsahu11 <amit.sahu@baelworks.com>
2022-01-14 16:15:29 +0530

    f

--- src/main/java/testfile/testfile/ControllerTest.java
+++ src/main/java/testfile/testfile/ControllerTest.java
@@ -1,7 +1,11 @@
 package testfile.testfile;
 
+import java.io.BufferedReader;
+
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +18,15 @@ public class ControllerTest {
         FileWriter myWriter = new FileWriter("filename.txt");
         myWriter.write("Files in Java might be tricky, but it is fun enough!" + test);
         myWriter.close();
-        return "Welcome";
+
+        // Note: Double backquote is to avoid compiler
+        // interpret words
+        // like \test as \t (ie. as a escape sequence)
+
+        // Creating an object of BufferedReader class
+        String data = "";
+        data = new String(Files.readAllBytes(Paths.get("filename.txt")));
+        return "Welcome" + data;
 
     }
 
